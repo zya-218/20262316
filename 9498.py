4b766611316ded7e2562c67c52a72ebbd5143764
zya-218 <nominzaya0208@gmail.com>
2026-03-21 22:12:03 +0900

    baekjoon 9498

---
+++ 9498.py
@@ -0,0 +1,11 @@
+grade = int(input())
+if grade>=90 and grade<=100:
+    print("A")
+elif grade<=89 and grade>=80:
+    print("B")
+elif grade<=79 and grade>=70:
+    print("C")
+elif grade<=69 and grade>=60:
+    print("D")
+else:
+    print("F")
